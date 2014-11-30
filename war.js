422b040a551b32b553b013fa3b312d5db2da4e95
Daniel <danielallendeutsch@gmail.com>
2014-11-30 14:18:11 -0500

    complete working version. random pick for ties.

--- war.js
+++ war.js
@@ -3,9 +3,23 @@ Rounds = new Mongo.Collection("rounds");
 
 var cardValues = [14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 var cardSuits = ["Hearts", "Diamond", "Spades", "Clubs"];
-var cardName = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+var cardNames = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
 
 if (Meteor.isClient) {
+  function getRound () {
+		return Rounds.find({}, {sort: {createdAt: -1}}).fetch()[0];
+	}
+	function isPlayer1Turn () {
+    if (Rounds.find({}).count() === 0) {
+			return true
+		} else if (getRound().player1 !== null && getRound().player2 !== null) {
+			console.log(getRound());
+			return true
+		} else {
+			return false
+		}
+	}
+	
   Template.body.helpers({
     cards: function () {
       return Cards.find();
@@ -16,65 +30,41 @@ if (Meteor.isClient) {
     'click button': function () {
 			Meteor.call('removeAllCards');
       Meteor.call('removeAllRounds');
-
-			for (i=0; i<cardValues.length; i++) {
-				for (j=0; j<cardSuits.length; j++) {
-          var player = 'player'+((j % 2) + 1);
-					Cards.insert({name: cardName[i], value: cardValues[i], suit: cardSuits[j], owner: player});
-				}
-			}
+			Meteor.call('dealCards', cardValues, cardSuits, cardNames);
     }
   });
 
   Template.showcards.events({
     "click .play-card": function () {
-      if (!Rounds.findOne())  {
+      if (isPlayer1Turn())  {
 				if (Meteor.user().username === 'player1') {
-				  Rounds.insert({player1: this._id, player2: null});
+				  Rounds.insert({player1: this._id, player2: null, createdAt: new Date()});
 			  } else {
-				  alert('It is not your turn to play.  Please wait patiently :)');
+					Meteor.call('wrongTurnAlert');
 				}
-			} else if (Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player1 !== null && Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player2 === null) {
+			} else {
 				if (Meteor.user().username === 'player2') {
-				  Rounds.update(Rounds.find({}, {sort: {_id: -1}}).fetch()[0]._id, { $set: { player2: this._id} });
+				  Rounds.update(getRound()._id, { $set: { player2: this._id} });
 					
-					var thisround = Rounds.find({}, {sort: {_id: -1}}).fetch()[0]
-					var p1cardnum = thisround.player1;
-					var p2cardnum = thisround.player2;
+					var p1cardnum = getRound().player1;
+					var p2cardnum = getRound().player2;
 					var p1card = Cards.find({_id: p1cardnum}).fetch()[0];
 					var p2card = Cards.find({_id: p2cardnum}).fetch()[0];
 
 					if (p1card.value === p2card.value) {
-						alert('Tie')
-							if ((Math.floor(Math.random() * 6) + 1) % 2 === 0) {
-								Cards.update(p1cardnum, { $set: { owner: 'player1'} });
-								Cards.update(p2cardnum, { $set: { owner: 'player1'} });
-								Rounds.update(thisround._id, { $set: { winner: 'player1'} });
-					    } else {
-								Cards.update(p1cardnum, { $set: { owner: 'player2'} });
-								Cards.update(p2cardnum, { $set: { owner: 'player2'} });
-								Rounds.update(thisround._id, { $set: { winner: 'player2'} });
-							}
+						if ((Math.floor(Math.random() * 6) + 1) % 2 === 0) {
+							Meteor.call("updateWithWin", 'player1', getRound()._id, p1cardnum, p2cardnum);
+						} else {
+							Meteor.call("updateWithWin", 'player2', getRound()._id, p1cardnum, p2cardnum);
+						}
 					}
 					else if (p1card.value > p2card.value) {
-						alert('Player 1 Won That Round!');
-				    Cards.update(p1cardnum, { $set: { owner: 'player1'} });
-				    Cards.update(p2cardnum, { $set: { owner: 'player1'} });
-						Rounds.update(thisround._id, { $set: { winner: 'player1'} });
+						Meteor.call("updateWithWin", 'player1', getRound()._id, p1cardnum, p2cardnum);
 					} else {
-						alert('Player 2 Won That Round!');
-				    Cards.update(p1cardnum, { $set: { owner: 'player2'} });
-				    Cards.update(p2cardnum, { $set: { owner: 'player2'} });
-						Rounds.update(thisround._id, { $set: { winner: 'player2'} });
+						Meteor.call("updateWithWin", 'player2', getRound()._id, p1cardnum, p2cardnum);
 					}
 			  } else {
-				  alert('It is not your turn to play.  Please wait patiently :)');
-				}
-			} else if (Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player1 !== null && Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player2 !== null) {
-				if (Meteor.user().username === 'player1') {
-				  Rounds.insert({player1: this._id, player2: null});
-			  } else {
-				  alert('It is not your turn to play.  Please wait patiently :)');
+					Meteor.call('wrongTurnAlert');
 				}
 			}
     }
@@ -87,50 +77,62 @@ if (Meteor.isClient) {
 	});
 
 	Template.showresults.helpers({
-    countyou: function () {
-			var youUser = Meteor.user().username;
-			var count = Cards.find({owner: youUser}).count();
-			return count;
+    cardCount: function () {
+			return Cards.find({owner: Meteor.user().username}).count();
 		},
 		lastwinner: function() {
-      return Rounds.find({}, {sort: {_id: -1}}).fetch()[0].winner;
+      return getRound().winner;
 		},
 	  p1card: function() {
-			var thisround = Rounds.find({}, {sort: {_id: -1}}).fetch()[0]
-			var p1cardnum = thisround.player1;
-			var p1card = Cards.find({_id: p1cardnum}).fetch()[0];
-			if (thisround.player1 !== null && thisround.player2 !== null) {
+			var p1card = Cards.find({_id: getRound().player1}).fetch()[0];
+			if (isPlayer1Turn()) {
 				return p1card.name + " of " + p1card.suit;
 	    }
 		},
 	  p2card: function() {
-			var thisround = Rounds.find({}, {sort: {_id: -1}}).fetch()[0]
-			var p2cardnum = thisround.player2;
-			var p2card = Cards.find({_id: p2cardnum}).fetch()[0];
-			if (thisround.player1 !== null && thisround.player2 !== null) {
+			var p2card = Cards.find({_id: getRound().player2}).fetch()[0];
+			if (isPlayer1Turn()) {
 				return p2card.name + " of " + p2card.suit;
 	    }
 		}
 	});
 
 	Template.playerturn.helpers({
-    pt: function () {
-			if (!Rounds.findOne()) {
-        return 'player1';
-			} else if (Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player1 === null) {	
-				return 'player1';
-			} else if (Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player1 !== null && Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player2 === null) {
-				return 'player2';
-			} else if (Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player1 !== null && Rounds.find({}, {sort: {_id: -1}}).fetch()[0].player2 !== null) {
-				return 'player1';
-			}
+    playerTurn: function () {
+		 if (isPlayer1Turn()) {
+			 return 'Player 1'
+		 } else {
+			 return 'Player 2'
      }
+		}
 	});
 
 	Accounts.ui.config({
   	passwordSignupFields: "USERNAME_ONLY"
 	});
 }
+
+
+Meteor.methods({ 
+  updateWithWin: function (winnerName, roundID, p1CardID, p2CardID) { 
+		Cards.update(p1CardID, { $set: { owner: winnerName} });
+		Cards.update(p2CardID, { $set: { owner: winnerName} });
+		Rounds.update(roundID, { $set: { winner: winnerName} });
+	},
+	dealCards: function (cardValues, cardSuits, cardNames) {
+		for (i=0; i<cardValues.length; i++) {
+			for (j=0; j<cardSuits.length; j++) {
+				var player = 'player'+((j % 2) + 1);
+				Cards.insert({name: cardNames[i], value: cardValues[i], suit: cardSuits[j], owner: player});
+			}
+		}
+	},
+	wrongTurnAlert: function () {
+		alert('It is not your turn to play.  Please wait patiently :)');
+	}
+});
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function() {
